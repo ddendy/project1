d7c0c9fd0323308042809496f38989fff2b18b0e
Daniil Kantser <kancerdanya20001@gmail.com>
2023-04-03 16:43:45 +0300

    program code added

---
+++ project/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] array = {"hello", "-345", "world", "al", "me", "i", "love", "IT"};
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length > 3) array[i] = "";
+}
+
+Console.WriteLine(String.Join(" " , array));
